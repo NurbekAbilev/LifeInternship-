4cdc8ecf22df3473c229702d13f77df2502cf057
tzhmbtv <elnur9924@gmail.com>
2019-02-01 01:39:53 +0600

    Statistics debugging

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -10,10 +10,9 @@ class StatisticsController extends Controller
 {
     public function index(Request $request)
     {
-
         $intervals = [0, 7, 15, 30, 60];
-        $from = date('d.m.Y', strtotime("-10 years"));
-        $to = date('d.m.Y', strtotime("10 years"));
+        $from = date('d.m.Y', strtotime("-1 years"));
+        $to = date('d.m.Y', strtotime("+1 years"));
         $statistics = [
             array("key" => "Поступивших с $from по $to", "value" => Ticket::whereBetween('created_at', array($from, $to))->count()),
             array("key" => "Отвеченных с $from по $to", "value" => 0),
@@ -49,7 +48,7 @@ class StatisticsController extends Controller
             array("key" => "Отвеченных в Контроле Качества", "value" => 0),
             array("key" => "Отвеченных за все время", "value" => Ticket::where('ticket_status','<=',4)->count()),
             //array("key" => "Отвеченных за " . $intervals[1] . " минут", "value" => Ticket::where('answered_time' - 'created_at' <= 7)->count()),
-            array("key" => "Отвеченных за " . $intervals[1] . "-" . $intervals[2] . " минут", "value" => 0),
+           // array("key" => "Отвеченных за " . $intervals[1] . "-" . $intervals[2] . " минут", "value" => Ticket::whereBetween("answer_time", array("created_at",date(("Y-m-d H:i:s"),strtotime("+7 minutes",strtotime("created_at")))))->where("ticket_status", ">=", 4)->count()),
             array("key" => "Отвеченных за " . $intervals[2] . "-" . $intervals[3] . " минут", "value" => 0),
             array("key" => "Отвеченных за " . $intervals[3] . "-" . $intervals[4] . " минут", "value" => 0),
             array("key" => "Отвеченных через час", "value" => 0)
